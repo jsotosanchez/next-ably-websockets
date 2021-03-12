1b5e1030df75256670bbda1882213c43206ee6a8
Jose Soto <jm.soto.sanchez@gmail.com>
2021-03-12 12:20:29 -0300

    message text on child component and refactoring

--- components/Chat.jsx
+++ components/Chat.jsx
@@ -1,11 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Box,
-  FormControl,
-  InputGroup,
-  Input,
   Button,
-  InputRightAddon,
   Modal,
   ModalOverlay,
   ModalContent,
@@ -15,89 +11,66 @@ import {
   ModalCloseButton,
   useDisclosure,
 } from '@chakra-ui/react';
-import { ArrowUpIcon } from '@chakra-ui/icons';
 import { useChannel } from '@/hooks/useChannel';
 import { useAuth } from './AuthProvider';
 import ChatBox from './ChatBox';
 import { ABLY_NEW_MESSAGE } from '../constants';
+import MessageForm from './MessageForm';
 
-export default function Chat({ channelName, ...rest }) {
+export default function Chat({ channelName }) {
   const { user } = useAuth();
   const { isOpen, onOpen, onClose } = useDisclosure();
-  const [messageText, setMessageText] = useState('');
   const [chatHistory, setChatHistory] = useState([]);
 
-  const [channel, ably] = useChannel(channelName, ({ data: { messageText, user } }) => {
+  const callbackOnMessage = ({ data: { messageText, user } }) => {
     const history = chatHistory.slice(-50);
     const newMessage = { messageText, user };
     setChatHistory([...history, newMessage]);
-  });
+  };
+
+  const callbackGetHistory = ({ items }) => {
+    console.log('items on history', items);
+    const oldMessages = items.map(({ data: { messageText, user } }) => ({ messageText, user }));
+    setChatHistory([...oldMessages]);
+  };
+
+  const [channel] = useChannel(channelName, callbackOnMessage);
 
   const sendChatMessage = (messageText) => {
     channel.publish({
       name: ABLY_NEW_MESSAGE,
       data: { messageText, user: { email: user?.email, name: user?.displayName } },
     });
-    setMessageText('');
   };
 
-  const onFormSubmit = (event) => {
+  const onFormSubmit = (event, message) => {
     event.preventDefault();
     if (!user) {
       onOpen();
       return;
     }
-    if (!messageText) {
+    if (!message) {
       return;
     }
 
-    sendChatMessage(messageText);
+    sendChatMessage(message);
   };
-
-  // const handleKeyPress = (event) => {
-  //   if (event.charCode !== 13 || messageTextIsEmpty) {
-  //     return;
-  //   }
-  //   sendChatMessage(messageText);
-  //   event.preventDefault();
-  // };
-
-  // const messages = chatHistory.map((message, index) => {
-  //   const author = message.user?.email === user?.email ? 'me' : 'other';
-  //   return <span key={index}>{message.messageText + author}</span>;
-  // });
 
   return (
     <Box>
       <ChatBox messages={chatHistory} />
-      {/* <Box>{messages}</Box> */}
-      <Box as="form" onSubmit={onFormSubmit}>
-        <FormControl id="text">
-          <InputGroup>
-            <Input
-              value={messageText}
-              onChange={(e) => setMessageText(e.target.value)}
-              placeholder="write your message here!"
-            ></Input>
-            <InputRightAddon>
-              <Button type="submit" variant="ghost">
-                <ArrowUpIcon />
-              </Button>
-            </InputRightAddon>
-          </InputGroup>
-        </FormControl>
-      </Box>
+      <MessageForm handleSubmit={onFormSubmit} />
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Not too fast!</ModalHeader>
           <ModalCloseButton />
-          <ModalBody>You need to sign up to participate in the chat</ModalBody>
+          <ModalBody>You need to sign in to participate in the chat</ModalBody>
           <ModalFooter>
             <Button colorScheme="whatsapp" mr={3} onClick={onClose}>
               No, thanks.
             </Button>
-            <Button variant="ghost">Sign in</Button>
+            <Button variant="ghost">Sign me in!</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
