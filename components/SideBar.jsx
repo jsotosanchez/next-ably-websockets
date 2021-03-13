c5a16ea1769116598582b09fe4baf3d90783dcb7
Jose Soto <jm.soto.sanchez@gmail.com>
2021-03-13 09:17:11 -0300

    highlight active channel

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -8,16 +8,28 @@ const ABLY_CHANNEL_LIVING = 'living';
 const ABLY_CHANNEL_BACKYARD = 'backyard';
 
 export default function SideBar() {
-  const { setChannel } = useChannelContext();
+  const { channel, setChannel } = useChannelContext();
   return (
     <VStack minW="7em" h="100vh" borderBottomWidth="1px">
-      <Button my="0.5em" variant="link" onClick={() => setChannel(ABLY_CHANNEL_ENTRANCE)}>
+      <Button
+        my="0.5em"
+        variant={channel === ABLY_CHANNEL_ENTRANCE ? 'solid' : 'link'}
+        onClick={() => setChannel(ABLY_CHANNEL_ENTRANCE)}
+      >
         Entrance
       </Button>
-      <Button my="0.5em" variant="link" onClick={() => setChannel(ABLY_CHANNEL_LIVING)}>
+      <Button
+        my="0.5em"
+        variant={channel === ABLY_CHANNEL_LIVING ? 'solid' : 'link'}
+        onClick={() => setChannel(ABLY_CHANNEL_LIVING)}
+      >
         Living
       </Button>
-      <Button my="0.5em" variant="link" onClick={() => setChannel(ABLY_CHANNEL_BACKYARD)}>
+      <Button
+        my="0.5em"
+        variant={channel === ABLY_CHANNEL_BACKYARD ? 'solid' : 'link'}
+        onClick={() => setChannel(ABLY_CHANNEL_BACKYARD)}
+      >
         Backyard
       </Button>
     </VStack>
