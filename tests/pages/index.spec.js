616b7289ba7132fce0ae5fc6b8253b0ec84b2c06
Jose Soto <jm.soto.sanchez@gmail.com>
2021-03-14 12:49:25 -0300

    tests set up

--- tests/pages/index.spec.js
+++ tests/pages/index.spec.js
@@ -1,9 +1,10 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Index from '@/pages/index';
 
 describe('App', () => {
   it('renders without crashing', () => {
-    render(<Index />);
+    const result = render(<Index />);
     expect(screen.getByRole('heading', { name: 'heading' })).toBeInTheDocument();
   });
 });
